d39b4ba90252f4f0b8e6d033df1d5ae417e5030b
邦子 小小 <873516725@qq.com>
2023-01-29 11:41:18 +0800

    更改ip

--- Classes/scene/StartScene.cpp
+++ Classes/scene/StartScene.cpp
@@ -36,9 +36,9 @@ bool DemoScene::init() {
     PhysicsShapeCache::getInstance()->addShapesWithFile("demo_physics.plist");
 
     //
-    // Connection::instance()->open("ws://101.43.196.171:23482");
+    Connection::instance()->open("ws://101.42.237.241:23482");
 
-    Connection::instance()->open("ws://127.0.0.1:23482");
+    //Connection::instance()->open("ws://127.0.0.1:23482");
 
     auto visibleSize = Director::getInstance()->getVisibleSize();
 
@@ -51,8 +51,8 @@ bool DemoScene::init() {
     this->schedule(
         [&, la](float) {
             if (Connection::instance()->is_error()) {
-                //  Connection::instance()->open("ws://101.43.196.171:23482");
-                Connection::instance()->open("ws://127.0.0.1:23482");
+                  Connection::instance()->open("ws://101.42.237.241:23482");
+                //Connection::instance()->open("ws://127.0.0.1:23482");
             } else if (Connection::instance()->is_open()) {
                 this->unschedule("check_server_online");
                 this->init_after_connect();
